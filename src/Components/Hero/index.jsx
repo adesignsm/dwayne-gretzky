271bba6811fa291016b9017a2d67d16561a307f7
Akash Mulye <akashmulye97@gmail.com>
2024-01-29 18:28:39 -0500

    responsive change

--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react';
+
 import './index.css';
 
 import DesktopLogo from '../../Assets/logos/desktop_logo.png';
+import MobileLogo from '../../Assets/logos/mobile_logo.png';
 
 export const Hero = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const handleWindowResize = () => {
+        setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        }
+    }, []);
+
+    const isMobile = width <= 768;
+
     return (
         <>
             <div className="hero">
                 <div className='logo'>
-                    <img src={DesktopLogo} />
+                    <img src={!isMobile ? DesktopLogo : MobileLogo} />
                 </div>
             </div>
         </>
