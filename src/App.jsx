bf9721e94f89d443b6c34b4d4e9540a371d0845c
Akash Mulye <akashmulye97@gmail.com>
2024-01-29 13:44:40 -0500

    gigwell booking form initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from './Components/Header';
 import { Hero } from './Components/Hero';
 import { ShowsTable } from './Components/ShowsTable';
+import { GigwellBookingForm } from './Components/GigWellBookingForm';
 
 import './root.css';
 
@@ -11,6 +12,7 @@ export const App = () => {
                 <Header />
                 <Hero />
                 <ShowsTable />
+                <GigwellBookingForm />
             </main>
         </>
     )
