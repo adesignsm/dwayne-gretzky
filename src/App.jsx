6e6856f56e614dded7ce5765dc8885e33541f925
Akash Mulye <akashmulye97@gmail.com>
2024-02-15 14:37:38 -0500

    booking page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Home } from "./Routes/Home";
 import { Tickets } from "./Routes/Tickets";
+import { Booking } from "./Routes/Booking";
 
 import { Header } from './Components/Header';
 import { Footer } from './Components/Footer';
@@ -17,6 +18,7 @@ export const App = () => {
                     <Routes>
                         <Route exact path='/' element={<Home />} />
                         <Route path='/tickets' element={<Tickets />} />
+                        <Route path='/booking' element={<Booking />} />
                     </Routes>
                     <Footer />
                 </BrowserRouter>
